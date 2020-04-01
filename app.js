1c9c9a01b2d67c8b4fad2b7a86a3724447d7d19c
Leder Diez <lederdiez00@gmail.com>
2020-04-01 15:03:23 -0500

    Route to https

--- app.js
+++ app.js
@@ -60,6 +60,15 @@ app.use(express.static('views'));
 
 var notification;
 
+// Route https
+app.use(function (req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+        return res.redirect('https://' + req.get('Host') + req.url);
+    }
+    next();    
+});
+
 // routers
 app.use("/", web_router);
 app.use("/api", api_router);
